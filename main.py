5c5b48e0d0749f6f3d60d09471a3a4891e15bbb1
alastergrume <kalinin.i.n@gmail.com>
2024-01-04 03:02:51 +0500

    add new app

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit VERSION 2.0!
+# Welcome to Streamlit VERSION 3.0!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
